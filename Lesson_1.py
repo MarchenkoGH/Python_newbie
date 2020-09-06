061c6431ebbd0fab239c4c7491688e1a7b3e60e7
MarchenkoGH <70818224+MarchenkoGH@users.noreply.github.com>
2020-09-06 11:11:16 +0300

    Add files via upload

---
+++ Lesson_1.py
@@ -0,0 +1,30 @@
+# VARIANT 1
+#
+# print("       *     *      * * *  *   *     ")
+# print("      * *    *      *       * *      ")
+# print("     *   *   *      * * *    *       ")
+# print("    * * * *  *   *  *       * *      ")
+# print("   *       * * * *  * * *  *   *     ")
+
+# VARIANT 2 - the following code is working good in Terminal only
+#
+# from art import *
+# art_name = text2art("ALEXANDER", font="caligraphy")
+# print(art_name)
+
+
+# print('"\\a"     Bell (alert)')
+# print('"\\b"     Backspace')
+# print('"\\n"     New line')
+# print('"\\t"     Horizontal tab')
+# print('\\\       Backslash \\')
+# print('\\”       Double quotation mark“')
+# print("\\’       Single quotation mark‘")
+
+
+# Number1 = int(input("please insert Number 1: "))
+# Number2 = int(input("please insert Number 2: "))
+# print("Number 1 + Number 2 =", Number1 + Number2)
+# print("Number 1 // Number 2 =", Number1 // Number2)
+# print("Number 1 % Number 2 =", Number1 % Number2)
+# print("Number 1 ** Number 2 =", Number1 ** Number2)
