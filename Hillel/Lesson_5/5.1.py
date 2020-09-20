0b86ae50ac5f13893253bd652961daef518dc801
MarchenkoGH <70818224+MarchenkoGH@users.noreply.github.com>
2020-09-20 22:33:56 +0300

    Update 5.1.py

--- Hillel/Lesson_5/5.1.py
+++ Hillel/Lesson_5/5.1.py
@@ -5,8 +5,6 @@ number = int(input())
 def is_prime(number) -> bool:
     if number == 1:
         return False
-    elif number == 2:
-        return True
     for i in range(2, int(math.sqrt(number)) + 1):
         if number % i == 0:
             return False
