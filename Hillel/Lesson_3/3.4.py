53c163de4be1242362c8777b9828cb98619ecaaa
MarchenkoGH <70818224+MarchenkoGH@users.noreply.github.com>
2020-09-20 18:28:19 +0300

    Create 3.4.py

---
+++ Hillel/Lesson_3/3.4.py
@@ -0,0 +1,6 @@
+list_of_six = [100, 106, 112, 118, 124, 130, 136, 142, 148, 154, 160, 166, 172, 178, 184, 190, 196]
+for number in list_of_six:
+    if number % 5 == 0:
+        print(number)
+    if number > 150:
+        break
