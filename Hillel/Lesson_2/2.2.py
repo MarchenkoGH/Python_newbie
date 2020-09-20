6f47e60428b4626df9d3aec73fa77404ad0a80ea
MarchenkoGH <70818224+MarchenkoGH@users.noreply.github.com>
2020-09-20 18:24:03 +0300

    Create 2.2.py

---
+++ Hillel/Lesson_2/2.2.py
@@ -0,0 +1,3 @@
+number = int(input("Please enter an integer number: "))
+print("The next number for the number", number, "is", number + 1)
+print("The previous number for the number", number, "is", number - 1)
