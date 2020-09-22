364743ae775ad0494d595d1bc031daa6a65ecbb7
MarchenkoGH <70818224+MarchenkoGH@users.noreply.github.com>
2020-09-22 22:43:10 +0300

    Update 3.3.py

--- Hillel/Lesson_3/3.3.py
+++ Hillel/Lesson_3/3.3.py
@@ -1,3 +1,6 @@
-list_ten = [10, 20, 30, 40, 50]
-print(list_ten.reverse()) # не хочет печатать list_ten
+ist_ten = [10, 20, 30, 40, 50]  # 1st option
+# list_ten.reverse()
+# print(list_ten)
+
+list_ten = [10, 20, 30, 40, 50]  # 2nd option
 print(list_ten[::-1])
