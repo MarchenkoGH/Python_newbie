ab9014406c2863abd7bfdf11dc32aff92c471bc6
MarchenkoGH <70818224+MarchenkoGH@users.noreply.github.com>
2020-09-22 21:25:28 +0300

    Update 3.2.py

--- Hillel/Lesson_3/3.2.py
+++ Hillel/Lesson_3/3.2.py
@@ -1,4 +1,11 @@
-x = float(input('введите число в формате ##.##: '))
-y = x - x // 1
-y = "{:.2f}".format(y)
-print(y)
+# x = float(input('введите число в формате ##.##: '))
+# y = x - x // 1
+# y = "{:.2f}".format(y)
+# print(y)
+ 
+# замечание принял, исправил решение т.к. не правильно понял задание
+
+x = float(input('Please input number in following format ##.##: '))
+fractional_part = int(x * 100 % 100)
+second_number = int(x * 10 % 10)
+print(fractional_part, second_number, sep="\n")
