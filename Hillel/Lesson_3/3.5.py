2be6d9b28d3b740623cf40c31b850d60f0fdc7e3
MarchenkoGH <70818224+MarchenkoGH@users.noreply.github.com>
2020-09-23 20:51:15 +0300

    Update 3.5.py

--- Hillel/Lesson_3/3.5.py
+++ Hillel/Lesson_3/3.5.py
@@ -1,9 +1,16 @@
+# Lesson 3.5 - Теперь цикл работает верно
 import random
 x2 = random.randint(1, 10)
-i = 3:
-for i < 3:
-    x1 = int(input('введите число от 1 до 10: '))
-        print('ты ввел:', x1)
-    i -= 1
-else:
-print('You won!') if x1 == x2 else print('You lose!')
+i = 0
+while i < 3:
+    x1 = int(input('please insert number in range 1 - 10: '))
+    if x1 == x2:
+        print(f'Computer inserted {x2}, You won!')
+        break
+    else:
+        i += 1
+        if i == 3:
+            print(f'You have spent 3 attempts already, Computer inserted {x2}, bye!')
+            break
+        else:
+            print('You you did not guess, try again!')
