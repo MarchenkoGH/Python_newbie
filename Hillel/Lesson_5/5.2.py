16b2833dbd465f71c72fc4603ea2a4423d48b42f
MarchenkoGH <70818224+MarchenkoGH@users.noreply.github.com>
2020-09-23 21:33:09 +0300

    Update 5.2.py

--- Hillel/Lesson_5/5.2.py
+++ Hillel/Lesson_5/5.2.py
@@ -1,15 +1,15 @@
-some_text = input('Введите какой то текст: ')
-print(f'Количество символов в тексте: {len(some_text)} \nКоличество слов в тексте: {len(some_text.split())}')
+some_text = input('Please enter any text: ')
+print(f'Number of characters in this text: {len(some_text)} \nNumber of words in this text: {len(some_text.split())}')
 
 # second code
 letters = 0
 spaces = 0
-some_text = input('Введите какой то текст: ')
+some_text = input('Please enter any text: ')
 symbols_in_test = len(some_text)
 for i in range(len(some_text)):
     if some_text[i] == " ":
         spaces += 1
     else:
         letters += 1
-print(f'Количество символов в тексте: {symbols_in_test} \nКоличество слов в тексте : {spaces + 1}')
-print(f'Количество букв в тексте : {letters} \nКоличество пробелов в тексте: {spaces}')
+print(f'Number of symbols in this text: {symbols_in_test} \nNumber of words in this text : {spaces + 1}')
+print(f'Number of characters in this text : {letters} \nNumber of spases in this text: {spaces}')
