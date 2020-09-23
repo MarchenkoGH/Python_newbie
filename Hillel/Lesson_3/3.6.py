2949bdca2cc5983cde35478a2e2f9057f7b7fbcd
MarchenkoGH <70818224+MarchenkoGH@users.noreply.github.com>
2020-09-23 20:52:47 +0300

    Update 3.6.py

--- Hillel/Lesson_3/3.6.py
+++ Hillel/Lesson_3/3.6.py
@@ -1,7 +1,7 @@
-x1 = int(input())
-y1 = int(input())
-x2 = int(input())
-y2 = int(input())
+x1 = abs(int(input()))
+y1 = abs(int(input()))
+x2 = abs(int(input()))
+y2 = abs(int(input()))
 if x2 - x1 == 2 and y2 - y1 == 1:
     print('yes')
 else:
