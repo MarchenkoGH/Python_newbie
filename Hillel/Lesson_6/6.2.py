aa5df17938a28f6e361bdc84d948056f38cb812d
MarchenkoGH <70818224+MarchenkoGH@users.noreply.github.com>
2020-10-01 01:16:04 +0300

    Update 6.2.py

--- Hillel/Lesson_6/6.2.py
+++ Hillel/Lesson_6/6.2.py
@@ -1,13 +1,14 @@
 import time
 
 
-
-def what_time_is_it_now():
+def countdown():
     secunds = 3
     for i in range(1, secunds + 1):
         print(i)
         time.sleep(1)
-    return time.strftime("%H:%M")
 
 
-print(what_time_is_it_now())
+
+@countdown()
+def what_time_is_it_now():
+    print(time.strftime("%H:%M"))
