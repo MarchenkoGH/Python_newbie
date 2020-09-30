03fa0cfe90bddb9d8f92def6bf0ecb8da60473c5
MarchenkoGH <70818224+MarchenkoGH@users.noreply.github.com>
2020-09-30 22:48:25 +0300

    Update 6.1.py

--- Hillel/Lesson_6/6.1.py
+++ Hillel/Lesson_6/6.1.py
@@ -1,1 +1,7 @@
+ef common_dict():
+    return {coin: code for coin, code in zip(coin, code)}
 
+
+coin = ('Bitcoin', 'Ether', 'Ripple', 'Litecoin')
+code = ('BTC', 'ETH', 'XRP', 'LTC')
+print(common_dict())
