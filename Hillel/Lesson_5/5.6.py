95e78823f1f72885491e85a874f3b561894c4106
MarchenkoGH <70818224+MarchenkoGH@users.noreply.github.com>
2020-09-30 21:32:38 +0300

    Update 5.6.py

--- Hillel/Lesson_5/5.6.py
+++ Hillel/Lesson_5/5.6.py
@@ -1,3 +1,7 @@
+def dict_without_empty_values(my_dict):
+    new_dict = {key: values for key, values in my_dict.items() if values != None}
+    return new_dict
+
+
 my_dict = {'first_color': 'Red', 'second_color': 'Green', 'third_color': None}
-new_dict = {key: values for key, values in my_dict.items() if values != None}
-print(new_dict)
+print(dict_without_empty_values(my_dict))
